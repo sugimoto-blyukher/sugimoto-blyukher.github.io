8960fb38193ec80d24067bed2ce6ab74ce113c0a
sinta <ramenrice555@gmail.com>
2025-12-03 08:18:13 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  site: 'sinta.fun',
+  site: 'https://sinta.fun/',
   base: '/',
   adapter: cloudflare(),
   integrations: [relativeLinks()],
